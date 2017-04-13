051b6f3eb85c085092341dd85e57fb79eb685228
Harry Nurbudyana <harrynurbudyana@users.noreply.github.com>
2017-04-13 11:52:30 +0700

    Add files via upload

---
+++ interface.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void inter_login()
+{
+	cout << "======================================================================" << "\n";
+	cout << "========================  ASSALAMUALAIKUM  ===========================" << "\n";
+	cout << "========   Selamat Datang di Aplikasi Penjadwalan Kajian  ============" << "\n";
+	cout << "===============   Silakan Login Terlebih Dahulu   ====================" << "\n";
+	cout << "======================================================================" << "\n";
+}
+
+int main()
+{
+	inter_login();
+}
---
+++ interface.h
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void inter_login()
+{
+	cout << ">>>>>>>>>>>>>>>>>>>>>>>>  ASSALAMUALAIKUM  <<<<<<<<<<<<<<<<<<<<<<<<<<<" << "\n";
+	cout << ">>>>>>>>   Selamat Datang di Aplikasi Penjadwalan Kajian  <<<<<<<<<<<<" << "\n";
+	cout << ">>>>>>>>>>>>>>    Silakan Login Terlebih Dahulu   <<<<<<<<<<<<<<<<<<<<" << "\n";
+}
+
+int main()
+{
+	inter_login();
+}
