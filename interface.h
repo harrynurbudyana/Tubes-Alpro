b1660ed285ff9be0edbe850cbea4a105a7f67bb1
Harry Nurbudyana <harrynurbudyana@users.noreply.github.com>
2017-04-13 13:24:00 +0700

    Add files via upload

--- interface.h
+++ interface.h
@@ -3,12 +3,24 @@ using namespace std;
 
 void inter_login()
 {
-	cout << ">>>>>>>>>>>>>>>>>>>>>>>>  ASSALAMUALAIKUM  <<<<<<<<<<<<<<<<<<<<<<<<<<<" << "\n";
-	cout << ">>>>>>>>   Selamat Datang di Aplikasi Penjadwalan Kajian  <<<<<<<<<<<<" << "\n";
-	cout << ">>>>>>>>>>>>>>    Silakan Login Terlebih Dahulu   <<<<<<<<<<<<<<<<<<<<" << "\n";
+	cout << "======================================================================" << "\n";
+	cout << "========================  ASSALAMUALAIKUM  ===========================" << "\n";
+	cout << "========   Selamat Datang di Aplikasi Penjadwalan Kajian  ============" << "\n";
+	cout << "===============   Silakan Login Terlebih Dahulu   ====================" << "\n";
+	cout << "======================================================================" << "\n";
 }
 
+void usr_menu()
+{
+	cout << "======================================================================" << "\n";
+	cout << "============================  Menu Pengguna  =========================" << "\n";
+	cout << " 1. Pencarian Pengisi Kajian " << "\n";
+	cout << " 2. Pencarian Berdasarkan Tanggal " << "\n";
+	cout << " 3. Pencarian Berdasarkan Waktu " << "\n";
+	cout << " 4. Pengurutan Berdasarkan Tanggal dan Waktu " << "\n";
+}
 int main()
 {
-	inter_login();
+//	inter_login();
+	usr_menu();
 }
